1f191e806d8c01f6e4d15914f7ac577030c7bb08
Hristo Yonkov <hristopturs@gmail.com>
2022-07-24 22:43:36 +0300

    add wizard to the screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-alert('hello from main js')
+const gameStart = document.querySelector('.game-start')
+const gameArea = document.querySelector('.game-area')
+const gameOver = document.querySelector('.game-over')
+const gameScore = document.querySelector('.game-score')
+
+gameStart.addEventListener('click', onGameStart);
+
+function onGameStart() {
+    gameStart.classList.add('hide')
+
+    //render wizard
+    const wizard = document.createElement('div');
+    wizard.classList.add('wizard');
+    wizard.style.top = '200px';
+    wizard.style.left = '200px';
+    gameArea.appendChild(wizard);
+
+}
+
+//global key listeners
