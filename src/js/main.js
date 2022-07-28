f744d923ceef56c940953f5e791466fe78530fbc
Hristo Yonkov <hristopturs@gmail.com>
2022-07-28 16:17:35 +0300

    moving wizard

--- src/js/main.js
+++ src/js/main.js
@@ -7,35 +7,61 @@ gameStart.addEventListener('click', onGameStart);
 
 function onGameStart() {
     gameStart.classList.add('hide')
-
+    
     //render wizard
     const wizard = document.createElement('div');
     wizard.classList.add('wizard');
-    wizard.style.top = '200px';
-    wizard.style.left = '200px';
+    wizard.style.top = player.y + 'px';
+    wizard.style.left = player.x + 'px';
     gameArea.appendChild(wizard);
+    
+    //game infinite loop
+    window.requestAnimationFrame(gameAction)
+}
 
+let keys = {};
+
+let player = {
+    x: 150,
+    y: 100
+};
+
+let game = {
+    speed: 2
+};
+
+// Game loop function
+function gameAction() {
+    const wizard = document.querySelector('.wizard');
+    // Register user input
+    if (keys.ArrowUp) {
+        player.y -= game.speed;
+    }
+    if (keys.ArrowDown) {
+        player.y += game.speed;
+    }
+    if (keys.ArrowLeft) {
+        player.x -= game.speed;
+    }
+    if (keys.ArrowRight) {
+        player.x += game.speed;
+    }
+
+    wizard.style.top = player.y + 'px';
+    wizard.style.left = player.x + 'px';
+
+    window.requestAnimationFrame(gameAction)
 }
 
 //global key listeners
 document.addEventListener('keydown', onKeyDown);
 document.addEventListener('keyup', onKeyUp);
 
-let keys = {};
 
 function onKeyDown(e) {
-    keys[e.code] = true
-    console.log(keys);
-
+    keys[e.code] = true;
 }
 
 function onKeyUp(e) {
-    keys[e.code] = false
-    console.log(keys);
-
-}
-
-function gameAction() {
-    console.log('action');
-    window.requestAnimationFrame(gameAction)
+    keys[e.code] = false;
 }
