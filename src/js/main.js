8acf842314ee76c3f9f48ccd18af9ec0302e20c9
Hristo Yonkov <hristopturs@gmail.com>
2022-07-30 10:34:59 +0300

    add score functionallyty

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
-const gameStart = document.querySelector('.game-start')
-const gameArea = document.querySelector('.game-area')
-const gameOver = document.querySelector('.game-over')
-const gameScore = document.querySelector('.game-score')
+const gameStart = document.querySelector('.game-start');
+const gameArea = document.querySelector('.game-area');
+const gameOver = document.querySelector('.game-over');
+const gameScore = document.querySelector('.game-score');
+const gamePoints = document.querySelector('.points');
 
 gameStart.addEventListener('click', onGameStart);
 
 function onGameStart() {
-    gameStart.classList.add('hide')
+    gameStart.classList.add('hide');
     
     //render wizard
     const wizard = document.createElement('div');
@@ -35,10 +36,16 @@ let game = {
     speed: 2,
     speedMult: 4
 };
+let scene = {
+    score: 0
+}
 
 // Game loop function
 function gameAction() {
     const wizard = document.querySelector('.wizard');
+    
+    //Increment score count
+    scene.score++;
 
     // Apply gravitation
     let isInAir = (player.height + player.y) <= gameArea.offsetHeight;
@@ -62,6 +69,9 @@ function gameAction() {
 
     wizard.style.top = player.y + 'px';
     wizard.style.left = player.x + 'px';
+
+    // Apply score
+    gamePoints.textContent = scene.score;
 
     window.requestAnimationFrame(gameAction)
 }
